59cc7321a97408ec2b64c08467c3158544c8cb6f
Myles Broomes <mylesbroomes@hotmail.co.uk>
2020-12-24 22:52:38 +0000

    Add marker to map

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,13 @@
 let map;
 
 function initMap() {
+  const location = { lat: 50.92731651229432, lng: -1.3312037236649066 };
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 50.92731651229432, lng: -1.3312037236649066 },
-    zoom: 15,
+    zoom: 16,
+  });
+  const marker = new google.maps.Marker({
+    position: location,
+    map: map,
   });
 }
